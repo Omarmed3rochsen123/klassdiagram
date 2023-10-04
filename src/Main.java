446c0b5b5b6cadd14fc69ffbb3f4ef63ff957cc8
Omarmed3rochsen123 <leo.almeblad@elev.ga.ntig.se>
2023-10-04 11:04:02 +0200

    okok idk

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        author emil = new author("emil","emil.emilson",'M');
+        System.out.println(emil.getName());
+        System.out.println(emil.toString());
+    }
+}
---
+++ src/author.java
@@ -0,0 +1,32 @@
+public class author {
+    private String name;
+    private String email;
+    private char gender;
+
+
+    public author(String name,String email,char gender){
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public String getName() { return name; }
+
+    public String getEmail() { return email; }
+
+    public char getGender() { return gender; }
+
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
+
+
+    public String toString() {
+        return "author{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
