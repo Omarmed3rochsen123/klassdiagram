51d7e2f26cf92d61011ff511f5f7e5fbe36c982b
Omarmed3rochsen123 <leo.almeblad@elev.ga.ntig.se>
2023-10-23 09:41:10 +0200

    check char

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
-import java.util.Scanner;
+import java.util.Scanner
+
 
 public class Main {
+    static char gender;
+    static String subGend;
 
     public static void main(String[] args) {
 
+
         String name;
         String mail;
-        char gend;
+        String gend;
 
         Scanner keyboard = new Scanner(System.in);
         System.out.print("Skriv in ditt namn: ");
@@ -14,12 +18,12 @@ public class Main {
         System.out.print("Skriv in din mail: ");
         mail = keyboard.nextLine();
         System.out.print("Skriv in ditt kön, M elr F: ");
-
+        gend = keyboard.nextLine();
 
 
         System.out.println("Hello world!");
 
-        author user1 = new author(name,mail,'M');
+        author user1 = new author(name,mail,gender);
         System.out.println(user1);
 
         author hamood = new author("hamood", "hamood.habibi", 'M');
@@ -27,5 +31,16 @@ public class Main {
 
         write abc = new write("abc");
         System.out.println(abc);
+    }
+
+    public char toChar(String gend){
+        if (gend.length() > 1){
+            System.out.println("Input för lång, endast en bokstav");
+            return 'ö';
+        } else {
+            subGend = String.valueOf(gend.indexOf(0,0));
+
+            return gender;
+        }
     }
 }
